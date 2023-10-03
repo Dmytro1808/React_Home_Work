9d8c5f82cc3c6a74052a419bb543b2523da8fc94
Dmytro <darknegs8@gmail.com>
2023-10-03 18:11:35 +0300

    сhanged

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,24 @@
-import './Card.css'
-import Input from '../../components/Input/Input'
+import './Card.css';
+import React from 'react';
+import Input from "../../components/Input/Input.js";
+import InputPassword from "../../components/InputPassword/InputPassword.js";
+import Button from "../../components/Button/Button.js";
 
-function Login() {
+function Card() {
   return (
-    <div>
-      <Input />
-    </div>
+    <form>
+      <h1 className='logo-name'>Harvey Norman</h1>
+      <div className="input-box">
+        <Input />
+      </div>
+      <div className="input-box">
+        <InputPassword />
+      </div>
+      <Button />
+    </form>
   );
 }
 
-export default Login;
+export default Card;
 
 
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,21 +1,10 @@
-import Input from "../../components/Input/Input.js";
-import InputPassword from "../../components/InputPassword/InputPassword.js";
-import Button from "../../components/Button/Button.js";
+import Card from "../../components/Card/Card.js";
 import "./Login.css";
 
 function Login() {
   return (
     <div className="wrapper">
-      <form >
-        <h1>Harvey Norman</h1>
-        <div className="input-box">
-          <Input />
-        </div>
-        <div className="input-box">
-          <InputPassword />
-        </div>
-        <Button />
-      </form>
+   <Card/>
     </div>
   );
 }
