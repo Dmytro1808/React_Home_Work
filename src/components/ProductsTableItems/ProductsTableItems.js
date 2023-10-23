29c7395a6b66069fe3aedecef2d6f0455de6a2c7
Dmytro <darknegs8@gmail.com>
2023-10-23 23:19:17 +0300

    UpdateError

--- src/components/ProductsTableItems/ProductsTableItems.js
+++ src/components/ProductsTableItems/ProductsTableItems.js
@@ -4,10 +4,11 @@ import { FaPen, FaShoppingBasket } from "react-icons/fa";
 import { API } from "../../constance/URL";
 import WindowDelete from "../WindowDelete/WindowDelete";
 
-function ProductsTableItems() {
+function ProductsTableItems({ setIsDeleteModalVisible, setIsBasketClicked }) {
   const [products, setProducts] = useState([]);
   const [isDeleteModalVisible, setDeleteModalVisible] = useState(false);
   const [deleteItemId, setDeleteItemId] = useState(null);
+  const [isTableBlurred, setTableBlurred] = useState(false);
 
   const deleteProduct = async (id) => {
     await fetch(`${API}/${id}`, {
@@ -23,25 +24,16 @@ function ProductsTableItems() {
 
   const openDeleteModal = (id) => {
     setDeleteItemId(id);
-    const modal = document.getElementById("modalId");
-    if (modal) {
-      modal.classList.remove("hidden");
-    }
-
     setDeleteModalVisible(true);
-    document.querySelector(".table").classList.add("blur-background");
-    document.querySelector(".header").classList.add("blur-background");
-    document.querySelector(".button-div").classList.add("blur-background");
+    setTableBlurred(true);
+    setIsBasketClicked(true);
   };
+
   const closeDeleteModal = () => {
-    const modal = document.getElementById("modalId");
-    if (modal) {
-      modal.classList.add("hidden");
-    }
+    setTableBlurred(false);
     setDeleteModalVisible(false);
-    document.querySelector(".table").classList.remove("blur-background");
-    document.querySelector(".header").classList.remove("blur-background");
-    document.querySelector(".button-div").classList.remove("blur-background");
+    setIsDeleteModalVisible(false);
+    setIsBasketClicked(false);
   };
 
   useEffect(() => {
@@ -55,90 +47,90 @@ function ProductsTableItems() {
 
   return (
     <>
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>
-                <span>ID</span>
-                <button className="sort-button">
-                  <span id="sort-icon-up">&#8593;</span>
-                </button>
-                <button className="sort-button">
-                  <span id="sort-icon-down">&#8595;</span>
-                </button>
-              </th>
-              <th>
-                <span>Category</span>
-                <button className="sort-button">
-                  <span id="sort-icon-up">&#8593;</span>
-                </button>
-                <button className="sort-button">
-                  <span id="sort-icon-down">&#8595;</span>
-                </button>
-              </th>
-              <th>
-                <span>Name</span>
-                <button className="sort-button">
-                  <span id="sort-icon-up">&#8593;</span>
-                </button>
-                <button className="sort-button">
-                  <span id="sort-icon-down">&#8595;</span>
-                </button>
-              </th>
-              <th>
-                <span>Quantity</span>
-                <button className="sort-button">
-                  <span id="sort-icon-up">&#8593;</span>
-                </button>
-                <button className="sort-button">
-                  <span id="sort-icon-down">&#8595;</span>
-                </button>
-              </th>
-              <th>
-                <span>Price $</span>
-              </th>
-              <th></th>
+      <table className={`table ${isTableBlurred ? "blur-background" : ""}`}>
+        <thead>
+          <tr>
+            <th>
+              <span>ID</span>
+              <button className="sort-button">
+                <span id="sort-icon-up">&#8593;</span>
+              </button>
+              <button className="sort-button">
+                <span id="sort-icon-down">&#8595;</span>
+              </button>
+            </th>
+            <th>
+              <span>Category</span>
+              <button className="sort-button">
+                <span id="sort-icon-up">&#8593;</span>
+              </button>
+              <button className="sort-button">
+                <span id="sort-icon-down">&#8595;</span>
+              </button>
+            </th>
+            <th>
+              <span>Name</span>
+              <button className="sort-button">
+                <span id="sort-icon-up">&#8593;</span>
+              </button>
+              <button className="sort-button">
+                <span id="sort-icon-down">&#8595;</span>
+              </button>
+            </th>
+            <th>
+              <span>Quantity</span>
+              <button className="sort-button">
+                <span id="sort-icon-up">&#8593;</span>
+              </button>
+              <button className="sort-button">
+                <span id="sort-icon-down">&#8595;</span>
+              </button>
+            </th>
+            <th>
+              <span>Price $</span>
+            </th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <span>{product.id}</span>
+              </td>
+              <td>
+                <span>{product.category}</span>
+              </td>
+              <td>
+                <span>{product.name}</span>
+              </td>
+              <td>
+                <span>{product.quantity}</span>
+              </td>
+              <td>
+                <span>{product.price}</span>
+              </td>
+              <td>
+                <span className="actions">
+                  <FaPen className="w-[40px] delete-change" />
+                  <FaShoppingBasket
+                    className="w-[40px] delete-btn"
+                    onClick={() => {
+                      openDeleteModal(product.id);
+                    }}
+                  />
+                </span>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product.id}>
-                <td>
-                  <span>{product.id}</span>
-                </td>
-                <td>
-                  <span>{product.category}</span>
-                </td>
-                <td>
-                  <span>{product.name}</span>
-                </td>
-                <td>
-                  <span>{product.quantity}</span>
-                </td>
-                <td>
-                  <span>{product.price}</span>
-                </td>
-                <td>
-                  <span className="actions">
-                    <FaPen className="w-[40px] delete-change" />
-                    <FaShoppingBasket
-                      className="w-[40px] delete-btn"
-                      onClick={() => openDeleteModal(product.id)}
-                    />
-                  </span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {isDeleteModalVisible && (
-          <WindowDelete
-            closeModal={closeDeleteModal}
-            onClick={() => deleteProduct(deleteItemId)}
-          />
-        )}
-      </div>
+          ))}
+        </tbody>
+      </table>
+      {isDeleteModalVisible && (
+        <WindowDelete
+          closeModal={closeDeleteModal}
+          onClick={() => deleteProduct(deleteItemId)}
+        />
+      )}
     </>
   );
 }
--- src/containers/ProductTable/ProductTable.js
+++ src/containers/ProductTable/ProductTable.js
@@ -4,19 +4,24 @@ import { VscAdd } from "react-icons/vsc";
 import ProductsTableItems from "../../components/ProductsTableItems/ProductsTableItems";
 import ButtonPreview from "../../components/ButtonPreview/ButtonPreview";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 function ProductTable() {
   const preview = <FaRegUser className="FaRegUser " />;
   const add = <VscAdd className="VscAdd" />;
-
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(true);
+  const [isBasketClicked, setIsBasketClicked] = useState(false);
   return (
     <>
       <div className="header">
-        <Link to="/login" className="logo-link">
+        <Link
+          to="/login"
+          className={`logo-link ${isBasketClicked ? "blur-background" : ""}`}
+        >
           Harvey Norman
         </Link>
       </div>
-      <div className="button-div">
+      <div className={`button-div ${isBasketClicked ? "blur-background" : ""}`}>
         <ButtonPreview title="Preview" icon={preview} />
         <ButtonPreview title="Add product" icon={add} />
       </div>
@@ -26,7 +31,10 @@ function ProductTable() {
         </h1>
       </div>
       <div className="flex justify-center items-center mt-[40px] mb-[75px]">
-        <ProductsTableItems />
+        <ProductsTableItems
+          setIsDeleteModalVisible={setIsDeleteModalVisible}
+          setIsBasketClicked={setIsBasketClicked}
+        />
       </div>
     </>
   );
